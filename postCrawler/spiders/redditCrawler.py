63d9314be56807233910c36f9e8852aafdf7761b
Atharva <atharvakn@gmail.com>
2019-09-10 20:04:30 +0530

    Fixed the credits for posts

--- postCrawler/spiders/redditCrawler.py
+++ postCrawler/spiders/redditCrawler.py
@@ -15,14 +15,15 @@ class RedditcrawlerSpider(scrapy.Spider):
         l = [x for x in l if 'i.redd.it' in x or 'external-preview.redd.it' in x]
         if len(l)==0:
             return
-        author = response.xpath("//a[@class='_2tbHP6ZydRpjI44J3syuqC _23wugcdiaj44hdfugIAlnX oQctV4n0yUb0uiHDdGnmE']/text()").get()
+        #author = response.xpath("//a[contains(@class,'_2tbHP6ZydRpjI44J3syuqC _23wugcdiaj44hdfugIAlnX oQctV4n0yUb0uiHDdGnmE')]/text()").getall()
+        author = response.xpath("//*/div/div[2]/div/div[1]/div/a/text()").get()
         with open('img_urls.txt', 'a+') as f:
             f.write("{}^^{}^^{}\n".format(l[0],author,response.url))
 
 
     def parse(self, response):
         l = response.xpath('//a/@href').getall()
-        l = [x for x in l if 'reddit' in x and 'comments/c' in x]
+        l = [x for x in l if 'reddit' in x and 'comments/d' in x]
 
         for i in l:
             yield scrapy.Request(url=i,callback=self.getImageUrl)
